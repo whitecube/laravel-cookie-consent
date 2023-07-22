85dbb3500dec039f57138e0134cfce95a68484cf
eldIsher <138291070+eldIsher@users.noreply.github.com>
2023-07-22 11:29:03 +0200

    reset all cookies with queue

--- src/Http/Controllers/ResetController.php
+++ src/Http/Controllers/ResetController.php
@@ -2,20 +2,34 @@
 
 namespace Whitecube\LaravelCookieConsent\Http\Controllers;
 
-use Whitecube\LaravelCookieConsent\CookiesManager;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Http\Request;
+use Whitecube\LaravelCookieConsent\CookiesManager;
+use Whitecube\LaravelCookieConsent\Facades\Cookies;
 
-class ResetController
-{
-    public function __invoke(Request $request, CookiesManager $cookies)
-    {
-        $response = ! $request->expectsJson()
+
+class ResetController {
+
+    public function __invoke(Request $request, CookiesManager $cookies) {
+        $response = !$request->expectsJson()
             ? redirect()->back()
             : response()->json([
                 'status' => 'ok',
                 'scripts' => $cookies->getNoticeScripts(true),
                 'notice' => $cookies->getNoticeMarkup(),
             ]);
+
+        $domain = config('cookieconsent.cookie.domain');
+
+        // delete all defined cookies
+        foreach (Cookies::getCategories() as $category) {
+            foreach ($category->getCookies() as $cookie) {
+                Cookie::queue(Cookie::forget(
+                    name: $cookie->name,
+                    domain: $domain,
+                ));
+            }
+        }
 
         return $response->withoutCookie(
             cookie: config('cookieconsent.cookie.name'),
