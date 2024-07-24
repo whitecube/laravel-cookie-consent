6f6ebbc78594518e21e6370f7e960a4c42960e03
Toon Van den Bos <toon@whitecube.be>
2024-07-24 18:00:02 +0200

    Fixed code style from #45

--- src/AnalyticCookiesCategory.php
+++ src/AnalyticCookiesCategory.php
@@ -15,29 +15,29 @@ class AnalyticCookiesCategory extends CookiesCategory
         $this->group(function (CookiesGroup $group) use ($id) {
             $key = (strpos($id, 'G-') === 0) ? substr($id, 2) : $id;
             $group->name(static::GOOGLE_ANALYTICS)
-                ->cookie(
-                    fn (Cookie $cookie) => $cookie->name('_ga')
+                ->cookie(function (Cookie $cookie) {
+                    $cookie->name('_ga')
                         ->duration(2 * 365 * 24 * 60)
-                        ->description(__('cookieConsent::cookies.defaults._ga'))
-                )
-                ->cookie(
-                    fn (Cookie $cookie) => $cookie->name('_ga_' . strtoupper($key))
+                        ->description(__('cookieConsent::cookies.defaults._ga'));
+                })
+                ->cookie(function (Cookie $cookie) {
+                    $cookie->name('_ga_' . strtoupper($key))
                         ->duration(2 * 365 * 24 * 60)
-                        ->description(__('cookieConsent::cookies.defaults._ga_ID'))
-                )
-                ->cookie(
-                    fn (Cookie $cookie) => $cookie->name('_gid')
+                        ->description(__('cookieConsent::cookies.defaults._ga_ID'));
+                })
+                ->cookie(function (Cookie $cookie) {
+                    $cookie->name('_gid')
                         ->duration(24 * 60)
-                        ->description(__('cookieConsent::cookies.defaults._gid'))
-                )
-                ->cookie(
-                    fn (Cookie $cookie) => $cookie->name('_gat')
+                        ->description(__('cookieConsent::cookies.defaults._gid'));
+                })
+                ->cookie(function (Cookie $cookie) {
+                    $cookie->name('_gat')
                         ->duration(1)
-                        ->description(__('cookieConsent::cookies.defaults._gat'))
-                )
+                        ->description(__('cookieConsent::cookies.defaults._gat'));
+                })
                 ->accepted(function (Consent $consent) use ($id) {
-                    $consent->script('<script async src="https://www.googletagmanager.com/gtag/js?id=' . $id . '"></script>')
-                        ->script('<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag(\'js\',new Date());gtag(\'config\',\'' . $id . '\');</script>');
+                    $consent->script('<script async src="https://www.googletagmanager.com/gtag/js?id='.$id.'"></script>')
+                        ->script('<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag(\'js\',new Date());gtag(\'config\',\''.$id.'\');</script>');
                 });
         });
 
@@ -51,11 +51,11 @@ class AnalyticCookiesCategory extends CookiesCategory
     {
         $this->group(function (CookiesGroup $group) use ($key) {
             $group->name(static::FACEBOOK_PIXEL)
-                ->cookie(
-                    fn (Cookie $cookie) => $cookie->name('fbpx')
+                ->cookie(function (Cookie $cookie) {
+                    $cookie->name('fbpx')
                         ->duration(2 * 365 * 24 * 60)
-                        ->description(__('cookieConsent::cookies.defaults.fbpx'))
-                )
+                        ->description(__('cookieConsent::cookies.defaults.fbpx'));
+                })
                 ->accepted(function (Consent $consent) use ($key) {
                     $consent->script('<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,\'script\',\'https://connect.facebook.net/en_US/fbevents.js\');fbq(\'init\', \'' . $key . '\');fbq(\'track\', \'PageView\');</script>');
                 });
