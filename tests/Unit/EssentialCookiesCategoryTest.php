d1356adc17bf394c8edabd521775233f5c6a5d6f
Adrien Leloup <adrien@whitecube.be>
2025-02-24 21:24:17 +0100

    Update EssentialCookiesCategoryTest.php

--- tests/Unit/EssentialCookiesCategoryTest.php
+++ tests/Unit/EssentialCookiesCategoryTest.php
@@ -9,6 +9,7 @@ it('can register consent cookie', function () {
     Config::shouldReceive('get')->with('app.fallback_locale')->andReturn('en');
     Config::shouldReceive('get')->with('database.default')->andReturn('test');
     Config::shouldReceive('get')->with('database.connections.test')->andReturn(null);
+    Config::shouldReceive('get')->with('logging.channels.deprecations')->andReturn(null);
     Config::shouldReceive('get')->once()->with('cookieconsent.cookie.name')->andReturn('foo_consent');
     Config::shouldReceive('get')->once()->with('cookieconsent.cookie.duration')->andReturn(365 * 24 * 60);
 
@@ -25,6 +26,7 @@ it('can register session cookie', function () {
     Config::shouldReceive('get')->with('app.fallback_locale')->andReturn('en');
     Config::shouldReceive('get')->with('database.default')->andReturn('test');
     Config::shouldReceive('get')->with('database.connections.test')->andReturn(null);
+    Config::shouldReceive('get')->with('logging.channels.deprecations')->andReturn(null);
     Config::shouldReceive('get')->once()->with('session.cookie')->andReturn('foo_session');
     Config::shouldReceive('get')->once()->with('session.lifetime')->andReturn(120);
 
@@ -41,6 +43,7 @@ it('can register csrf cookie', function () {
     Config::shouldReceive('get')->with('app.fallback_locale')->andReturn('en');
     Config::shouldReceive('get')->with('database.default')->andReturn('test');
     Config::shouldReceive('get')->with('database.connections.test')->andReturn(null);
+    Config::shouldReceive('get')->with('logging.channels.deprecations')->andReturn(null);
     Config::shouldReceive('get')->once()->with('session.lifetime')->andReturn(120);
 
     $category = new EssentialCookiesCategory('foo');
