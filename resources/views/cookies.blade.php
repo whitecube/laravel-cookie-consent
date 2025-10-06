772a2dffc0d4c8f1ba7db5a954139434684a27b9
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-10-06 12:26:14 +0200

    update view to pre-check categories depending of the user preferences

--- resources/views/cookies.blade.php
+++ resources/views/cookies.blade.php
@@ -1,4 +1,5 @@
-<aside id="cookies-policy" class="cookies cookies--no-js" data-text="{{ json_encode(__('cookieConsent::cookies.details')) }}">
+<aside id="cookies-policy" class="cookies cookies--no-js"
+       data-text="{{ json_encode(__('cookieConsent::cookies.details')) }}">
     <div class="cookies__alert">
         <div class="cookies__container">
             <div class="cookies__wrapper">
@@ -15,48 +16,62 @@
                 </div>
             </div>
         </div>
-            <a href="#cookies-policy-customize" class="cookies__btn cookies__btn--customize">
-                <span>@lang('cookieConsent::cookies.customize')</span>
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path d="M14.7559 11.9782C15.0814 11.6527 15.0814 11.1251 14.7559 10.7996L10.5893 6.63297C10.433 6.47669 10.221 6.3889 10 6.38889C9.77899 6.38889 9.56703 6.47669 9.41075 6.63297L5.24408 10.7996C4.91864 11.1251 4.91864 11.6527 5.24408 11.9782C5.56951 12.3036 6.09715 12.3036 6.42259 11.9782L10 8.40074L13.5774 11.9782C13.9028 12.3036 14.4305 12.3036 14.7559 11.9782Z" fill="#2C2E30"/>
-                </svg>
-            </a>
+        <a href="#cookies-policy-customize" class="cookies__btn cookies__btn--customize">
+            <span>@lang('cookieConsent::cookies.customize')</span>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"
+                 aria-hidden="true">
+                <path d="M14.7559 11.9782C15.0814 11.6527 15.0814 11.1251 14.7559 10.7996L10.5893 6.63297C10.433 6.47669 10.221 6.3889 10 6.38889C9.77899 6.38889 9.56703 6.47669 9.41075 6.63297L5.24408 10.7996C4.91864 11.1251 4.91864 11.6527 5.24408 11.9782C5.56951 12.3036 6.09715 12.3036 6.42259 11.9782L10 8.40074L13.5774 11.9782C13.9028 12.3036 14.4305 12.3036 14.7559 11.9782Z"
+                      fill="#2C2E30"/>
+            </svg>
+        </a>
         <div class="cookies__expandable cookies__expandable--custom" id="cookies-policy-customize">
             <form action="{{ route('cookieconsent.accept.configuration') }}" method="post" class="cookies__customize">
                 @csrf
                 <div class="cookies__sections">
+                    @php
+                        $cookieName = config('cookieconsent.cookie.name');
+                        $cookieValue = $_COOKIE[$cookieName] ?? null;
+                        $preferences = $cookieValue ? json_decode($cookieValue) : [];
+                    @endphp
                     @foreach($cookies->getCategories() as $category)
-                    <div class="cookies__section">
-                        <label for="cookies-policy-check-{{ $category->key() }}" class="cookies__category">
-                            @if ($category->key() === 'essentials')
-                                <input type="hidden" name="categories[]" value="{{ $category->key() }}" />
-                                <input type="checkbox" name="categories[]" value="{{ $category->key() }}" id="cookies-policy-check-{{ $category->key() }}" checked="checked" disabled="disabled" />
-                            @else
-                                <input type="checkbox" name="categories[]" value="{{ $category->key() }}" id="cookies-policy-check-{{ $category->key() }}" />
-                            @endif
-                            <span class="cookies__box">
+                        @php
+                            $isChecked = !empty($preferences['categories'][$category->key()] ?? null);
+                        @endphp
+                        <div class="cookies__section">
+                            <label for="cookies-policy-check-{{ $category->key() }}" class="cookies__category">
+                                @if ($category->key() === 'essentials')
+                                    <input type="hidden" name="categories[]" value="{{ $category->key() }}"/>
+                                    <input type="checkbox" name="categories[]" value="{{ $category->key() }}"
+                                           id="cookies-policy-check-{{ $category->key() }}" checked="checked"
+                                           disabled="disabled"/>
+                                @else
+                                    <input type="checkbox" name="categories[]" value="{{ $category->key() }}"
+                                           id="cookies-policy-check-{{ $category->key() }}" @checked($isChecked)/>
+                                @endif
+                                <span class="cookies__box">
                                 <strong class="cookies__label">{{ $category->title }}</strong>
                             </span>
-                            @if($category->description)
-                                <p class="cookies__info">{{ $category->description }}</p>
-                            @endif
-                        </label>
+                                @if($category->description)
+                                    <p class="cookies__info">{{ $category->description }}</p>
+                                @endif
+                            </label>
 
-                        <div class="cookies__expandable" id="cookies-policy-{{ $category->key() }}">
-                            <ul class="cookies__definitions">
-                                @foreach($category->getCookies() as $cookie)
-                                <li class="cookies__cookie">
-                                    <p class="cookies__name">{{ $cookie->name }}</p>
-                                    <p class="cookies__duration">{{ \Carbon\CarbonInterval::minutes($cookie->duration)->cascade() }}</p>
-                                    @if($cookie->description)
-                                        <p class="cookies__description">{{ $cookie->description }}</p>
-                                    @endif
-                                </li>
-                                @endforeach
-                            </ul>
+                            <div class="cookies__expandable" id="cookies-policy-{{ $category->key() }}">
+                                <ul class="cookies__definitions">
+                                    @foreach($category->getCookies() as $cookie)
+                                        <li class="cookies__cookie">
+                                            <p class="cookies__name">{{ $cookie->name }}</p>
+                                            <p class="cookies__duration">{{ \Carbon\CarbonInterval::minutes($cookie->duration)->cascade() }}</p>
+                                            @if($cookie->description)
+                                                <p class="cookies__description">{{ $cookie->description }}</p>
+                                            @endif
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <a href="#cookies-policy-{{ $category->key() }}"
+                               class="cookies__details">@lang('cookieConsent::cookies.details.more')</a>
                         </div>
-                        <a href="#cookies-policy-{{ $category->key() }}" class="cookies__details">@lang('cookieConsent::cookies.details.more')</a>
-                    </div>
                     @endforeach
                 </div>
                 <div class="cookies__save">
