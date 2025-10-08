427cdd8bfc087499c9234d605a0870537f0584d9
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-10-08 17:36:14 +0200

    Change withoutMiddleware method use for a better understanding

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,26 @@ use Whitecube\LaravelCookieConsent\Http\Controllers\ScriptController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\AcceptAllController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\ConfigureController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\AcceptEssentialsController;
+Route::withoutMiddleware([EncryptCookies::class])->group(function () {
+    Route::group([
+        'as' => 'cookieconsent.',
+        'domain' => config('cookieconsent.url.domain'),
+        'prefix' => config('cookieconsent.url.prefix'),
+        'middleware' => config('cookieconsent.url.middleware')
+    ], function() {
+        Route::get('script', ScriptController::class)
+            ->name('script');
 
-Route::group([
-    'as' => 'cookieconsent.',
-    'domain' => config('cookieconsent.url.domain'),
-    'prefix' => config('cookieconsent.url.prefix'),
-    'middleware' => config('cookieconsent.url.middleware')
-], function() {
-    Route::get('script', ScriptController::class)
-        ->name('script');
+        Route::post('accept-all', AcceptAllController::class)
+            ->name('accept.all');
 
-    Route::post('accept-all', AcceptAllController::class)
-        ->name('accept.all');
+        Route::post('accept-essentials', AcceptEssentialsController::class)
+            ->name('accept.essentials');
 
-    Route::post('accept-essentials', AcceptEssentialsController::class)
-        ->name('accept.essentials');
+        Route::post('configure', ConfigureController::class)
+            ->name('accept.configuration');
 
-    Route::post('configure', ConfigureController::class)
-        ->name('accept.configuration');
-
-    Route::post('reset', ResetController::class)
-        ->name('reset');
-})->withoutMiddleware([EncryptCookies::class]);
+        Route::post('reset', ResetController::class)
+            ->name('reset');
+    });
+});
