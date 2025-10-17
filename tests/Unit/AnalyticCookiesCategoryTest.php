15e1eee867d128a460c2f2bffa9cc0ecf2a7d85c
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-10-17 16:07:12 +0200

    fix the name of the googleAnalytics method in the test

--- tests/Unit/AnalyticCookiesCategoryTest.php
+++ tests/Unit/AnalyticCookiesCategoryTest.php
@@ -6,7 +6,7 @@ use Whitecube\LaravelCookieConsent\AnalyticCookiesCategory;
 it('can register Google Analytics configuration', function () {
     $category = new AnalyticCookiesCategory('foo');
 
-    expect($category->google('g-foo'))->toBe($category);
+    expect($category->googleAnalytics('g-foo'))->toBe($category);
     expect($group = ($category->getDefined()[0] ?? null))->toBeInstanceOf(CookiesGroup::class);
 
     expect($group->hasConsentCallback())->toBeTrue();
