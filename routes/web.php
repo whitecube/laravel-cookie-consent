1e816324789e1a1533b5a2712c511c0dd7100e3b
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-10-17 13:29:01 +0200

    Since the next major update will require laravel 12 we can disable cookie encryption for our cookie this way

--- routes/web.php
+++ routes/web.php
@@ -7,26 +7,25 @@ use Whitecube\LaravelCookieConsent\Http\Controllers\ScriptController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\AcceptAllController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\ConfigureController;
 use Whitecube\LaravelCookieConsent\Http\Controllers\AcceptEssentialsController;
-Route::withoutMiddleware([EncryptCookies::class])->group(function () {
-    Route::group([
-        'as' => 'cookieconsent.',
-        'domain' => config('cookieconsent.url.domain'),
-        'prefix' => config('cookieconsent.url.prefix'),
-        'middleware' => config('cookieconsent.url.middleware')
-    ], function() {
-        Route::get('script', ScriptController::class)
-            ->name('script');
 
-        Route::post('accept-all', AcceptAllController::class)
-            ->name('accept.all');
+Route::group([
+    'as' => 'cookieconsent.',
+    'domain' => config('cookieconsent.url.domain'),
+    'prefix' => config('cookieconsent.url.prefix'),
+    'middleware' => config('cookieconsent.url.middleware')
+], function () {
+    Route::get('script', ScriptController::class)
+        ->name('script');
 
-        Route::post('accept-essentials', AcceptEssentialsController::class)
-            ->name('accept.essentials');
+    Route::post('accept-all', AcceptAllController::class)
+        ->name('accept.all');
 
-        Route::post('configure', ConfigureController::class)
-            ->name('accept.configuration');
+    Route::post('accept-essentials', AcceptEssentialsController::class)
+        ->name('accept.essentials');
 
-        Route::post('reset', ResetController::class)
-            ->name('reset');
-    });
+    Route::post('configure', ConfigureController::class)
+        ->name('accept.configuration');
+
+    Route::post('reset', ResetController::class)
+        ->name('reset');
 });
